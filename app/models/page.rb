b7cd943d0220fac02f28c57c1ed136d15dd8548a
maxpleaner <maxpleaner@gmail.com>
2015-04-15 21:48:53 +0000

    change message for ERRNO ENT

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,11 +10,14 @@ class StaticPage
 				page_not_found(name, "Not in the DB")
 			end	
 		rescue Errno::ENOENT
-			page_not_found(name, "Not in the filesystem")
+			awaiting_moderation(name)
 		end
 	end
 	def self.page_not_found(name, extra_message="")
 		Page.new(name: name, content: "#{"Page not found".red} #{extra_message}")
+	end
+	def self.awaiting_moderation(name)
+		Page.new(name: name, content: "hidden pending moderator approval".blue)
 	end
 end
 
